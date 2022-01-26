df771686dabbbdf1579149e113340f3b1bfb567b
leaswaran1976 <leaswaran@live.com>
2022-01-26 11:45:38 -0500

    Added ApplicationScoped annotation

--- src/main/java/org/acndemo/statemachine/models/StateMachineRepository.java
+++ src/main/java/org/acndemo/statemachine/models/StateMachineRepository.java
@@ -2,8 +2,11 @@ package org.acndemo.statemachine.models;
 
 import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 
+@ApplicationScoped
 public class StateMachineRepository implements PanacheMongoRepository<StateMachineRecord> {
     public List<StateMachineRecord> findAllStateMachineRecords() {
         return listAll();
